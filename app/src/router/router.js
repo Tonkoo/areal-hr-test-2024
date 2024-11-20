8a2f46aee5c1ad1dd3418b4170b11d5db0bcc8bc
Tonkoo <andrey54374@gmail.com>
2024-11-20 19:24:54 +0300

    refactor: перемещён файл Positions.vue в папку pages

--- app/src/router/router.js
+++ app/src/router/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import Organizations from "../pages/Organizations.vue";
 import Departments from "../pages/Departments.vue";
-import Positions from "../components/Positions.vue";
+import Positions from "../pages/Positions.vue";
 import Employees from "../components/Employees.vue";
 import Operations from "../components/Operations.vue";
 
