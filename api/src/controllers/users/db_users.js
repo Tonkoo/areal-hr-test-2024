c6aeb937c43d789f4bac996f5089ebe574082cad
Tonkoo <andrey54374@gmail.com>
2024-11-26 19:21:35 +0300

    feat: реализовано шифрование пароля в виде Argon2id

--- api/src/controllers/users/db_users.js
+++ api/src/controllers/users/db_users.js
@@ -1,4 +1,5 @@
 const pool = require('../../db')
+const argon2 = require('argon2')
 
 async function getUsers() {
   const connection = await pool.connect()
@@ -18,10 +19,13 @@ async function getUsers() {
 async function addUser(last_name, first_name, middle_name, login, password) {
   const connection = await pool.connect()
   try {
+    const hashedPassword = await argon2.hash(password, {
+      type: argon2.argon2id,
+    })
     const result = await connection.query(
       `INSERT INTO users (last_name, first_name, middle_name, login, password, role_id) 
        VALUES ($1, $2, $3, $4, $5, 2) RETURNING *`,
-      [last_name, first_name, middle_name, login, password],
+      [last_name, first_name, middle_name, login, hashedPassword],
     )
     return result.rows[0]
   } catch (err) {
@@ -42,13 +46,16 @@ async function updateUser(
 ) {
   const connection = await pool.connect()
   try {
+    const hashedPassword = await argon2.hash(password, {
+      type: argon2.argon2id,
+    })
     const result = await connection.query(
       `UPDATE users 
        SET last_name = $1, first_name = $2,
        middle_name = $3, login = $4,
        password = $5
        WHERE id = $6 RETURNING *`,
-      [last_name, first_name, middle_name, login, password, id],
+      [last_name, first_name, middle_name, login, hashedPassword, id],
     )
     if (result.rows.length === 0) {
       return { error: 'User not found' }
